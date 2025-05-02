3b5ce56745304ae5a0e5636561cdbcc7f971b0f6
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2025-05-02 17:25:23 +0800

    page.tsx Retrieves products from API then stores in Redux state.

--- shopping/src/app/page.tsx
+++ shopping/src/app/page.tsx
@@ -1,40 +1,38 @@
+"use client";
+import { useEffect } from "react";
 import CategoricalFilter from "./components/CategoricalFilter";
-import ProductCard from "./components/ProductCard";
-import { Product } from "./types/product";
+import ProductGrid from "./components/ProductGrid";
+import axios from "axios";
+
+import { setProducts } from "@/redux/productSlice";
+import { useAppDispatch, useAppSelector } from "@/redux";
 
 export default function Home() {
-  const p1: Product = {
-    name: "Iphone 14 Pro ",
-    // name: "Iphone 14 Pro Iphone 14 Pro Iphone 14 Pro Iphone 14 Pro",
-    description: "Latest iphone by apple.",
-    id: 0,
-    price: 1400,
-    image: "An image of iphone",
-  };
-  const p2: Product = {
-    // name: "Iphone 14 Pro ",
-    name: "Iphone 14 Pro Iphone 14 Pro Iphone 14 Pro Iphone 14 Pro",
-    description: "Latest iphone by apple.",
-    id: 0,
-    price: 1400,
-    image: "An image of iphone",
-  };
+  const products = useAppSelector((state) => state.products.all);
+  const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    if (products.length == 0) {
+      axios
+        .get("https://fakestoreapi.com/products")
+        .then((response) => {
+          const prods = response.data;
+          // console.log(prods);
+          dispatch(setProducts(prods));
+        })
+        .catch((error) => {
+          console.log("Error occured when fetching from FakeStoreAPI:");
+          console.log(error);
+        });
+    }
+  });
   return (
     <div className="flex-1 flex flex-row p-0 m-0 border-0 border-blue-800 py-0">
       <div className="flex-1 grid grid-cols-1 auto-rows-min py-2 px-2 border-r-[0px] border-r-gray-300">
-        {/* <p>Categorical filter</p> */}
         <CategoricalFilter category="Product Category" />
         <CategoricalFilter category="Origin Country" />
       </div>
-      <div className="flex-[5] grid [grid-template-columns:repeat(auto-fill,_minmax(180px,_1fr))] auto-rows-[280px] gap-y-4 pl-4 pr-0 py-4 border-0 border-red-700">
-        <ProductCard product={p1}></ProductCard>
-        <ProductCard product={p1}></ProductCard>
-        <ProductCard product={p1}></ProductCard>
-        <ProductCard product={p2}></ProductCard>
-        <ProductCard product={p1}></ProductCard>
-        <ProductCard product={p1}></ProductCard>
-      </div>
+      <ProductGrid />
     </div>
   );
 }
