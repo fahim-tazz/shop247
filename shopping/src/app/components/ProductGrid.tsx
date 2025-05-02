80cb89fd39da7b9fb04dbd63e4cd883b01f6365a
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2025-05-02 19:08:31 +0800

    Add filtering logic to ProductGrid

--- shopping/src/app/components/ProductGrid.tsx
+++ shopping/src/app/components/ProductGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import { Product } from "../types/product";
 import { useAppSelector } from "@/redux";
@@ -8,7 +8,8 @@ type Props = {};
 export default function ProductGrid({}: Props) {
   const products = useAppSelector((state) => state.products.all);
   const loadingState = useAppSelector((state) => state.products.loading);
-
+  const filters = useAppSelector((state) => state.filters);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   // const p1: Product = {
   //   name: "Iphone 14 Pro ",
   //   // name: "Iphone 14 Pro Iphone 14 Pro Iphone 14 Pro Iphone 14 Pro",
@@ -25,8 +26,31 @@ export default function ProductGrid({}: Props) {
   //   price: 1400,
   //   image: "An image of iphone",
   // };
+  useEffect(() => {
+    const categoryFilter = filters.category;
+    const originFilter = filters.origin;
+    const priceRange = filters.chosenPriceRange;
 
-  const isEmptyList = products.length == 0;
+    const isCategoryFilterActive = Object.values(categoryFilter).some((v) => v);
+    const isOriginFilterActive = Object.values(originFilter).some((v) => v);
+
+    setFilteredProducts(
+      products.filter((product) => {
+        const categoryMatch =
+          !isCategoryFilterActive || categoryFilter[product.category];
+        const originMatch =
+          !isOriginFilterActive || originFilter[product.origin];
+        const priceMatch =
+          product.price >= priceRange[0] && product.price <= priceRange[1];
+
+        return categoryMatch && originMatch && priceMatch;
+      })
+    );
+
+    console.log("Filtered products:", filteredProducts);
+  }, [products, filters]);
+
+  const isEmptyList = filteredProducts.length == 0;
   return (
     <div className="flex-[5] grid [grid-template-columns:repeat(auto-fill,_minmax(180px,_1fr))] auto-rows-[280px] gap-y-4 pl-4 pr-0 py-4 border-0 border-red-700">
       {/* Loading state while fetching */}
@@ -39,7 +63,7 @@ export default function ProductGrid({}: Props) {
       {/* Fetched, products found: */}
       {!loadingState &&
         !isEmptyList &&
-        products
+        filteredProducts
           .slice(0, -1)
           .map((prod) => <ProductCard key={prod.id} product={prod} />)}
     </div>
