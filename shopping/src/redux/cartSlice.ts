ab13b5b9295a0583e4327c99bd0018ecde30869b
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2025-05-03 03:33:31 +0800

    Add action to update the quantity of one cart product.

--- shopping/src/redux/cartSlice.ts
+++ shopping/src/redux/cartSlice.ts
@@ -17,8 +17,20 @@ const cartSlice = createSlice({
     setCart(state, action: PayloadAction<Record<number, CartEntry>>) {
       state.products = action.payload;
     },
+    updateQuantity(
+      state,
+      action: PayloadAction<{ id: number; quantity: number }>
+    ) {
+      const { id, quantity } = action.payload;
+      if (quantity <= 0) {
+        delete state.products[id];
+      }
+      if (state.products[id]) {
+        state.products[id].quantity = quantity;
+      }
+    },
   },
 });
 
-export const { setCart } = cartSlice.actions;
+export const { setCart, updateQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
