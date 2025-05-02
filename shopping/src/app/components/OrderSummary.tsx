9fc83f9147eb60f148dfb875ad058a981e39dbf2
Muhammad Fahim Tajwar <fahim.tajwar@u.nus.edu>
2025-05-03 03:33:43 +0800

    Create OrderSummary component

---
+++ shopping/src/app/components/OrderSummary.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useAppSelector } from "@/redux";
+import React, { useState } from "react";
+
+type Props = {};
+
+export default function OrderSummary({}: Props) {
+  const cart = useAppSelector((state) => state.cart.products);
+
+  const subtotal = Object.values(cart).reduce(
+    (sum, { product, quantity }) => sum + product.price * quantity,
+    0
+  );
+
+  //   const [subtotal, setSubtotal] = useState(
+  //     Object.values(cart).reduce(
+  //       (sum, { product, quantity }) => sum + product.price * quantity,
+  //       0
+  //     )
+  const gst = subtotal * 0.09;
+  const deliveryFee = 2.99;
+  const total = subtotal + gst + deliveryFee;
+
+  return (
+    <div className=" p-4 rounded shadow bg-white">
+      <h2 className="text-lg font-semibold mb-4">Order Summary</h2>
+      <div className="flex justify-between text-sm mb-2">
+        <span>Subtotal:</span>
+        <span>${subtotal.toFixed(2)}</span>
+      </div>
+      <div className="flex justify-between text-sm mb-2">
+        <span>GST (9%):</span>
+        <span>${gst.toFixed(2)}</span>
+      </div>
+      <div className="flex justify-between text-sm mb-2">
+        <span>Delivery Fee:</span>
+        <span>${deliveryFee.toFixed(2)}</span>
+      </div>
+      <hr className="my-2" />
+      <div className="flex justify-between font-bold text-md">
+        <span>Total:</span>
+        <span>${total.toFixed(2)}</span>
+      </div>
+    </div>
+  );
+}
